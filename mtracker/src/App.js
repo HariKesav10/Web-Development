526bbceaa4263cc39b4ba0490c7ba4458bc05c88
Hari Kesav <110965514+programcraze@users.noreply.github.com>
2024-11-20 16:28:59 +0530

    First Commit

---
+++ mtracker/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg">
+      <p className="title">Mracker</p>
+      <p>This is the first scroll page</p>
+      </div>
+      <div className="sec">
+        <p>This is the second scroll page</p>
+      </div>
+      <header className="App-header">
+        <p>Hey there</p>
+
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
