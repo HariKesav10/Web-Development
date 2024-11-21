eb949bdcefa447fcd74e7e925b08e5d11fdae552
Hari Kesav <110965514+programcraze@users.noreply.github.com>
2024-11-21 17:18:00 +0530

    First commit for everything

--- mtracker/src/App.js
+++ mtracker/src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="bg">
+      <header className="App-header">
+        {/* <p>Hey there</p> */}
+        <div className="bg">
       <p className="title">Mracker</p>
       <p>This is the first scroll page</p>
       </div>
-      <div className="sec">
-        <p>This is the second scroll page</p>
-      </div>
-      <header className="App-header">
-        <p>Hey there</p>
-
       </header>
+      <div className="sec">
+        <div className='grid'>
+          <div className="fi"></div>
+          <p>The Wild Robot</p>
+        </div>
+      </div>
 
     </div>
   );
