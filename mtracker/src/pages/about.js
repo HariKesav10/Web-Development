4e0f0e2b63fd59d75b54211a0fa107752a0e436e
Hari Kesav <110965514+programcraze@users.noreply.github.com>
2025-06-18 22:04:15 +0530

    First Commit

---
+++ mtracker/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="page-content">
+      <h1>About Us</h1>
+      <p>Here you can learn more about our company.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
