8664baa6e2c0345e40126f69357f7e4ecdda5fb5
Hari Kesav <110965514+programcraze@users.noreply.github.com>
2025-06-18 22:06:09 +0530

    Second Commit

--- mtracker/src/App.js
+++ mtracker/src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar'; // Import the Navbar
+import HomePage from './pages/home';   // Import the Home page
+import AboutPage from './pages/about';  // Import the About page
+import './App.css'; // For general app styles
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <p>Hey there</p> */}
-        <div className="bg">
-      <p className="title">Mracker</p>
-      <p>This is the first scroll page</p>
+    <Router>
+      <div className="App">
+        <Navbar /> {/* <-- The Navbar is placed here, once! */}
+        <main>
+          <Routes> {/* The Routes component defines where page content will go */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
       </div>
-      </header>
-      <div className="sec">
-        <div className='grid'>
-          <div className="fi"></div>
-          <p>The Wild Robot</p>
-        </div>
-      </div>
-
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
